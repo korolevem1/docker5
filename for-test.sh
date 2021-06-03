e0adf36053ffe68988cbd5c2f20bf115be18841d
Eduard <korolev.e@inbox.ru>
2021-06-03 11:03:01 +0300

    add script for test

---
+++ for-test.sh
@@ -0,0 +1,13 @@
+apt update
+docker stop $(docker ps -a -q)
+docker container rm $(docker ps -a -q) -f
+docker image rm $(docker images -a -q) -f
+cd /root/docker5/
+git pull https://github.com/korolevem1/docker5.git
+docker build -t ubuntutom .
+docker images
+docker run -d -p 8080:8080 -t ubuntutom
+docker ps -a
+RUN /opt/tomcat/bin/catalina.sh run
+apt install nmap
+nmap 127.0.0.1
