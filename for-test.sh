a1c2053a0a3d84c21c6dae1f3bd64464e9da0a75
Eduard <korolev.e@inbox.ru>
2021-06-03 11:24:23 +0300

    remove from tomcat examples and docs, change in scripts name of container

--- for-test.sh
+++ for-test.sh
@@ -4,9 +4,9 @@ docker container rm $(docker ps -a -q) -f
 docker image rm $(docker images -a -q) -f
 cd /root/docker5/
 git pull https://github.com/korolevem1/docker5.git
-docker build -t ubuntutom .
+docker build -t alpinetom .
 docker images
-docker run -d -p 8080:8080 -t ubuntutom
+docker run -d -p 8080:8080 -t alpinetom
 docker ps -a
 RUN /opt/tomcat/bin/catalina.sh run
 apt install nmap
